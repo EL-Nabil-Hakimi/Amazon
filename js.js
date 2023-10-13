a112d7eae2b2e1588583a550908eee3b84543c8b
EL-Nabil-Hakimi <nabilelhakimi2023@gmail.com>
2023-10-13 12:25:38 +0100

    v15

---
+++ js.js
@@ -0,0 +1,10 @@
+var maDiv = document.getElementById("section1M");
+var BTN = document.getElementById("BTNN1");
+function showTable() {
+if(maDiv.style.display === "block"){
+    maDiv.style.display = "none";
+}
+else{
+    maDiv.style.display = "block";
+    
+}}
